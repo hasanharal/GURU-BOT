3cf9693fce3c64fa3202e8343fe10adb02a68fb4
hasanharal <122716449+hasanharal@users.noreply.github.com>
2023-12-14 17:13:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923326925115', 'GURU', true],
+  ['923326925115', 'Ash', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923326925115" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
