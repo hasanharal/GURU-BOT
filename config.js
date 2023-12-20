154f3bed5956b81b772796f10b7af892a78f562f
hasanharal <122716449+hasanharal@users.noreply.github.com>
2023-12-20 13:16:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "923057732219" //put your bot number here
+//global.pairingNumber = "923326925115" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
