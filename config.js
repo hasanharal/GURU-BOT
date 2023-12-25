7da25151164a8ee7b7d7dda289e3c6564a3a6fad
hasanharal <122716449+hasanharal@users.noreply.github.com>
2023-12-25 12:09:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'M Hassan Ramzan Haral' 
+global.author = 'M Hassan Ramzan Haral' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
